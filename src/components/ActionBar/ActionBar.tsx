aed640cc0d2a1d399abd346b77cde529b2b03717
Daphne Visser <daphnevis@gmail.com>
2022-05-12 17:10:11 +0200

    Add action bar component

---
+++ src/components/ActionBar/ActionBar.tsx
@@ -0,0 +1,19 @@
+import "./action-bar.css";
+import { useActions } from "../../hooks/useActions";
+import ActionButton from "./ActionButton";
+
+interface ActionBarProps {
+  id: string;
+}
+const ActionBar: React.FC<ActionBarProps> = ({ id }) => {
+  const { moveCell, deleteCell } = useActions();
+  return (
+    <div className="action-bar">
+      <ActionButton onClick={() => moveCell(id, "up")} icon="fa-arrow-up" />
+      <ActionButton onClick={() => moveCell(id, "down")} icon="fa-arrow-down" />
+      <ActionButton onClick={() => deleteCell(id)} icon="fa-times" />
+    </div>
+  );
+};
+
+export default ActionBar;
